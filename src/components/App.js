e2f5d1c169f7a0b7b9148639283dc5538aebb273
Ravi Prakash <120682913+raviprakash1998@users.noreply.github.com>
2023-05-21 20:51:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,31 @@ const App = () => {
   const [width, setWidth] = useState("");
   const [height, setHeight] = useState("");
   const [backgroundColor, setBackgroundColor] = useState("");
+  const [boxes, setBoxes] = useState([]);
+  
+  const handleWidthChange = (e) => {
+    setWidth(e.target.value);
+  }
+  
+  const handleHeightChange = (e) => {
+    setHeight(e.target.value);
+  }
+  
+  const handleBackgroundColorChange = (e) => {
+    setBackgroundColor(e.target.value);
+  }
+  
+  const handleCreateBox = () => {
+    const newBox = {
+      width,
+      height,
+      backgroundColor,
+    }
+    setBoxes(prevBox => [...prevBox, newBox]);
+    setWidth;
+    setHeight;
+    setBackgroundColor;
+  }
 
   return (
     <div id="main">
@@ -31,7 +56,19 @@ const App = () => {
         />
       </div>
       <button className='create-btn'>Create Box</button>
-      <div id="box-container"></div>
+      <div id="box-container">
+    {boxes.map((box, index) => (
+          <div
+            key={index}
+            className="box"
+            style={{
+              width: box.width,
+              height: box.height,
+              backgroundColor: box.backgroundColor,
+            }}
+          ></div>
+        ))}
+      </div>
     </div>
   );
 };
